544c29791732cf9894fbc4d778ed01cbd3ca31e8
ellisPae <ellis.h.pae@gmail.com>
2021-07-06 17:10:55 -0400

    Add tabs

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 
 
 import Clock from './clock';
+import Tabs from './tabs';
 
-
+const panes = [
+  {title: 'one', content: 'I am the first'},
+  {title: 'two', content: 'Second pane here'},
+  {title: 'three', content: 'Third pane here'}
+];
 
 function Root() {
   return(
     <div>
       <Clock />
       {/* <Weather /> */}
-      {/* <div className='interactive'>
+      <div className='interactive'>
         <Tabs panes={panes} />
-        <AutoComplete names={names} />
-      </div> */}
+        {/* <AutoComplete names={names} /> */}
+      </div>
     </div>
   );
 }
